0a035e9b2dbf1eda08649e2a18afe29e07079d77
Tasfik Rahman <tasfikrahman001@gmail.com>
2021-05-11 17:49:09 +0600

    dropdown effect added on projects

---
+++ App.js
@@ -0,0 +1,20 @@
+const totalProjects = 6;
+let revealed = [];
+
+for (let i = 0; i <= totalProjects; i++) {
+    revealed.push(false);
+}
+
+$(document).ready(function () {
+    for (let i = 1; i <= totalProjects; i++) {
+        let parentClass = ".project" + i;
+        let childClass = "#project-desc" + i;
+        $(parentClass).click(function () {
+            if (revealed[i]) {
+                $(childClass).slideUp("slow");
+            }
+            else $(childClass).slideDown("slow");
+            revealed[i] = !revealed[i];
+        });
+    }
+});
