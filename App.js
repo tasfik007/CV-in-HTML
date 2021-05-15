5fb2353812932b3a611b2c57e2f6cd28b96bb9c7
Tasfik Rahman <tasfikrahman001@gmail.com>
2021-05-15 22:42:46 +0600

    code optimized

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
-const totalProjects = 6;
-let revealed = [];
-
-for (let i = 0; i <= totalProjects; i++) {
-    revealed.push(false);
-}
-
 $(document).ready(function () {
-    for (let i = 1; i <= totalProjects; i++) {
-        let parentClass = ".project" + i;
-        let childClass = "#project-desc" + i;
-        $(parentClass).click(function () {
-            if (revealed[i]) {
-                $(childClass).slideUp("slow");
-            }
-            else {
-                $(childClass).slideDown("slow");
-            }
+    const TOTAL_PROJECTS = $(".projects").children().length;
+    for (let i = 1; i <= TOTAL_PROJECTS; i++) {
+        $(".project" + i).click(function () {
+            $("#project-desc" + i).toggle(500);
             $("#logo" + i).toggleClass('fa-plus fa-minus');
-            revealed[i] = !revealed[i];
         });
     }
 });
