772eca01aa5d02bfb8e9e35a6eabf2901056c5f0
Tasfik Rahman <tasfikrahman001@gmail.com>
2021-05-17 11:46:29 +0600

    code refactored

--- weather-api/weather.js
+++ weather-api/weather.js
@@ -5,27 +5,13 @@ var params = {
 };
 url.search = new URLSearchParams(params).toString();
 
-
 function kToC(temprature) {
     return (temprature - 273.15).toFixed(2);
 }
 function hPaToATM(pressure) {
     return (pressure * 0.000987).toFixed(2);
 }
-function getWeatherReport(weather) {
-    return `    
-                <i class="fas fa-city"></i>
-                <span style="font-size:30px;">${weather.city}</span> <br>
-                <i class="fas fa-sun"></i>
-                Current Temprature: ${weather.temprature.current}&#176;C <br>
-                <i class="fas fa-cloud-sun"></i>
-                Feels Like: ${weather.temprature.feels_like}&#176;C <br>
-                <i class="fas fa-wind"></i>
-                Wind Speed: ${weather.wind.speed} m/s <br>
-                <i class="fas fa-tint"></i>
-                Humidity: ${weather.humidity}%
-            `
-}
+
 function setWeatherReport(weatherData) {
     let children = $("#toast > span");
     for (let i = 0; i < children.length; i++) {
@@ -40,11 +26,8 @@ async function fetchWeatherData() {
     var raw_data = await fetch(url);
     var data = await raw_data.json();
     var weather = extractWeatherData(data);
-    console.log(weather);
-
     var x = document.getElementById("toast");
     setWeatherReport(weather);
-    //x.innerHTML = getWeatherReport(weather);
     let prev_class = x.className;
     x.className += " show";
     setTimeout(() => { x.className = x.className.replace(prev_class + " show", prev_class); }, 5000);
